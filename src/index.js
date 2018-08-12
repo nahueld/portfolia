3edfdd5817329fbbd799c54b1868fc2653707e3e
nahueld <nahuel.dealbera@gmail.com>
2018-08-12 11:44:04 +1000

    Removing service worker for easier testing

--- src/index.js
+++ src/index.js
@@ -4,7 +4,5 @@ import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 import "./index.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
